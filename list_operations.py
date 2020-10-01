04a1f9a39d391f6ae17b8042fa8cbbbd4b7cc14f
alexandramilliron <alexandramilliron@gmail.com>
2020-10-01 13:56:12 -0700

    partially completed functions

--- list_operations.py
+++ list_operations.py
@@ -3,14 +3,13 @@
 
 def head(input_list):
     """Return the first item of the input list.
-
+    return input(list)
     For example:
 
       >>> head(['Jan', 'Feb', 'Mar'])
       'Jan'
     """
-
-    return None
+    return input_list[0]
 
 
 def tail(input_list):
@@ -23,7 +22,7 @@ def tail(input_list):
 
     """
 
-    return []
+    return input_list[1:]
 
 
 def last(input_list):
@@ -36,7 +35,8 @@ def last(input_list):
 
     """
 
-    return []
+    return input_list[-1]
+
 
 
 def top(input_list):
@@ -49,7 +49,7 @@ def top(input_list):
 
     """
 
-    return []
+    return input_list[:-1]
 
 
 def first_three(input_list):
@@ -62,7 +62,7 @@ def first_three(input_list):
 
     """
 
-    return []
+    return input_list[:3]
 
 
 def last_five(input_list):
@@ -75,7 +75,7 @@ def last_five(input_list):
 
     """
 
-    return []
+    return input_list[-5:]
 
 
 def middle(input_list):
@@ -88,7 +88,7 @@ def middle(input_list):
 
     """
 
-    return []
+    return input_list[2:-2]
 
 
 def inner_four(input_list):
@@ -101,7 +101,7 @@ def inner_four(input_list):
 
     """
 
-    return []
+    return input_list[2:6]
 
 
 def inner_four_end(input_list):
@@ -117,7 +117,7 @@ def inner_four_end(input_list):
 
     """
 
-    return []
+    return input_list[-6:-2]
 
 
 def replace_head(input_list):
@@ -131,8 +131,7 @@ def replace_head(input_list):
     True
 
     """
-
-    pass
+    input_list[0] = 42
 
 
 def replace_third_and_last(input_list):
@@ -146,8 +145,9 @@ def replace_third_and_last(input_list):
     True
 
     """
+    input_list[2] = 37
+    input_list[-1] = 37
 
-    pass
 
 
 def replace_middle(input_list):
